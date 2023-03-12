e617fd07e4cdd482af9ab4dd137b80cfc9e3df85
Luca Bessi Aristei <luca.bessi.aristei@gmail.com>
2023-03-12 23:16:06 +0100

    Aggiunta about, aggiunta bibliografia, sistemati nomi, cambiate alcune id con data-*. Create le tooltip per i link

--- script.js
+++ script.js
@@ -14,14 +14,18 @@ $(document).ready(function () {
   });
 
   $('.nav-click').on('click', function () {
-    var $sublinksContainer = $(this).parent().find('.sublinks-container');
-    var sublinksContainerHeight = $sublinksContainer[0].scrollHeight;
+    if (!$(this).hasClass('extras')) {
+      var $sublinksContainer = $(this).parent().find('.sublinks-container');
+      var sublinksContainerHeight = $sublinksContainer[0].scrollHeight;
+    }
     if (!$(this).hasClass('active')) {
       // If this nav-click element is not active, close any other active nav-click elements and open this one
-      $('.nav-click.active').removeClass('active');
-      $('.nav-click-wrapper .sublinks-container').css('max-height', 0);
+      if (!$(this).hasClass('extras')) {
+        $('.nav-click.active').removeClass('active');
+        $('.nav-click-wrapper .sublinks-container').css('max-height', 0);
+        $sublinksContainer.css('max-height', sublinksContainerHeight + 'px');
+      }
       $(this).addClass('active');
-      $sublinksContainer.css('max-height', sublinksContainerHeight + 'px');
     }
   });
 
@@ -34,7 +38,58 @@ $(document).ready(function () {
     activeText.animate({
       scrollTop: activeText.find('[data-note-number="' + noteNumber + '"]').offset().top - activeText.offset().top + activeText.scrollTop() - 16
     }, 500);
-});
+  });
+
+
+  $('.text-container a').on('mouseenter', function (e) {
+    // create the tooltip box
+    var tooltip = $('<div class="tooltip"></div>');
+    // check if the link has a tooltip attribute
+    if ($(this).data('tooltip')) {
+      // add the tooltip text
+      var text = $('<span class="text"></span>');
+      text.text($(this).data('tooltip'));
+      tooltip.append(text);
+      // check if the tooltip contains an image
+      if ($(this).find('img').length) {
+        // add the image to the tooltip
+        var imgContainer = $('<span class="tooltip-image"></span>');
+        var img = $(this).find('img').clone();
+        imgContainer.append(img);
+        tooltip.prepend(imgContainer);
+      }
+      // add the tooltip to the page
+      $('body').append(tooltip);
+      // position the tooltip above the link
+      
+      var linkOffset = $(this).offset();
+      var linkWidth = $(this).outerWidth();
+      var linkHeight = $(this).outerHeight();
+      var tooltipWidth = tooltip.outerWidth();
+      var tooltipHeight = tooltip.outerHeight();
+      var top = Math.max(linkOffset.top - tooltipHeight, 0); // make sure top is not less than 0
+      var left = e.pageX; // use mouse position for left coordinate
+      if (left + tooltipWidth > $(window).width()) {
+        left = $(window).width() - tooltipWidth;
+      }
+
+
+      tooltip.css({
+        top: top,
+        left: left,
+        visibility: 'visible',
+        opacity: 1
+      });
+    }
+  }).on('mouseleave', function () {
+    // remove the tooltip from the page
+    $('.tooltip').fadeOut(100, function () {
+      $(this).remove();
+    });
+  });
+
+
+
 
 
 
